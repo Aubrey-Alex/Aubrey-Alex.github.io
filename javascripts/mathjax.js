557e73d398eee920562f74ed4bad6f9f021a0796
Aubrey-Alex <Aubrey-Alex@users.noreply.github.com>
2025-04-29 03:50:05 +0000

    Deployed d0199e5 with MkDocs version: 1.5.3

---
+++ javascripts/mathjax.js
@@ -0,0 +1,24 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+    macros: {
+      leftouterjoin: "\\mathbin{⟕}",
+      rightouterjoin: "\\mathbin{⟖}",
+      fullouterjoin: "\\mathbin{⟗}"
+    }
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => { 
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
